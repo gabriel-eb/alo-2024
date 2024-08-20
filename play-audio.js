458a591d6108213e180bd2ca43d34c1ab60c132b
gabriel-eb <gabo_espburg@hotmail.com>
2024-08-20 22:54:23 +0000

    Finish play audio btn

--- play-audio.js
+++ play-audio.js
@@ -1,9 +1,16 @@
 const customPlay = () => {
     const audio = document.getElementById("sample-audio");
+    const pause = document.getElementById("pause-btn");
+    const play = document.getElementById("play-btn");
+    
     console.log('play pressed');
     if (audio.paused) {
         audio.play();
+        pause.style.visibility = "visible";
+        play.style.visibility = "hidden";
     } else {
         audio.pause();
+        play.style.visibility = "visible";
+        pause.style.visibility = "hidden";
     }
 }
