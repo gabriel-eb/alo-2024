3ddaa7aec16ea8f0547a759d00ec926ba86dc7b1
gabriel-eb <gabo_espburg@hotmail.com>
2024-08-28 03:17:41 +0000

    rename and move files

--- felicidades.js
+++
@@ -1,7 +0,0 @@
-const onContinue = () => {
-    const firstPage = document.getElementById('intro');
-    const felicidades = document.getElementById('felicitaciones');
-    const swapContent = firstPage.innerHTML;
-    firstPage.innerHTML = felicidades.innerHTML;
-    felicidades.innerHTML = swapContent;
-}
