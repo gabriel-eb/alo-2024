9b8dd3a105e5c04d94165fedd91c6466c14e0fc9
gabriel-eb <gabo_espburg@hotmail.com>
2024-08-28 02:52:45 +0000

    Add more functionality to player

--- play-audio.js
+++ play-audio.js
@@ -1,8 +1,12 @@
-const customPlay = () => {
+const initElements = () => {
     const audio = document.getElementById("sample-audio");
     const pause = document.getElementById("pause-btn");
     const play = document.getElementById("play-btn");
-    
+    return { audio, pause, play };
+}
+
+const customPlay = () => {
+    const { audio, pause, play } = initElements();
     console.log('play pressed');
     if (audio.paused) {
         audio.play();
@@ -13,4 +17,11 @@ const customPlay = () => {
         play.style.visibility = "visible";
         pause.style.visibility = "hidden";
     }
+}
+
+const stopPlay = () => {
+    const { audio, pause, play } = initElements();
+    audio.pause();
+    play.style.visibility = "visible";
+    pause.style.visibility = "hidden";
 }
